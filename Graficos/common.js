238a3c4d7c7a78750b9a5d1c9ac1c75c73d9cfa4
sousats <111879026+sousats@users.noreply.github.com>
2024-10-21 09:30:50 -0300

    Criei os arquivos java

---
+++ Graficos/common.js
@@ -0,0 +1,12 @@
+
+const getCSS = (variavel) => {
+    return getComputedStyle(document.body).getPropertyValue(variavel)
+}
+
+const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
+}
+
+export { getCSS, tickConfig }
